c8ef492ed8ddc6c2da2dfbe9f995b4615af0f9c7
romanizat <romanizat@gmail.com>
2022-01-29 10:07:56 +0100

    added increment for position on new EventSuggestion save

--- src/main/java/romanizat/voxpopuli/repository/EventSuggestionRepository.java
+++ src/main/java/romanizat/voxpopuli/repository/EventSuggestionRepository.java
@@ -14,6 +14,18 @@ public interface EventSuggestionRepository extends JpaRepository<EventSuggestion
     @Query(value = "SELECT es.* " +
             "FROM event_suggestion es " +
             "WHERE es.id_event = :eventId order by es.position ", nativeQuery = true)
-    List<EventSuggestion> findAllEventSuggestionsForEvent(@Param("eventId") Integer eventId);
+    List<EventSuggestion> findAllEventSuggestionsForEventByEventIdOrderedByPosition(@Param("eventId") Integer eventId);
+
+//    @Query(value = "SELECT es.* " +
+//            "FROM event_suggestion es " +
+//            "WHERE es.id_event = :eventId and es.url = :url ", nativeQuery = true)
+//    EventSuggestion findAllEventSuggestionsForEvent(@Param("url") String url, @Param("eventId") Integer eventId);
+
+    List<EventSuggestion> findAllByEventId(Integer eventId);
+
+    @Query(value = "select max(es.position) " +
+            "from event_suggestion es " +
+            "where es.id_event = :eventId", nativeQuery = true)
+    Integer findMaxPositionForEventSuggestionByEventId(@Param("eventId") Integer eventId);
 
 }
--- src/main/java/romanizat/voxpopuli/service/impl/EventSuggestionServiceImpl.java
+++ src/main/java/romanizat/voxpopuli/service/impl/EventSuggestionServiceImpl.java
@@ -1,14 +1,15 @@
 package romanizat.voxpopuli.service.impl;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import romanizat.voxpopuli.entity.*;
+import romanizat.voxpopuli.entity.EventSuggestion;
 import romanizat.voxpopuli.exception.UrlExistsException;
 import romanizat.voxpopuli.repository.EventSuggestionRepository;
 import romanizat.voxpopuli.service.EventSuggestionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -40,6 +41,7 @@ public class EventSuggestionServiceImpl implements EventSuggestionService {
         if (songExistsInEvent(eventSuggestion.getUrl(), eventSuggestion.getEvent().getId())) {
             throw new UrlExistsException();
         }
+        eventSuggestion.setPosition(eventSuggestionRepository.findMaxPositionForEventSuggestionByEventId(eventSuggestion.getEvent().getId()) + 1);
 
         return eventSuggestionRepository.save(eventSuggestion);
     }
@@ -56,11 +58,11 @@ public class EventSuggestionServiceImpl implements EventSuggestionService {
 
     @Override
     public List<EventSuggestion> getAllEventSuggestionsForEvent(Integer idEvent) {
-        return eventSuggestionRepository.findAllEventSuggestionsForEvent(idEvent);
+        return eventSuggestionRepository.findAllEventSuggestionsForEventByEventIdOrderedByPosition(idEvent);
     }
 
     private boolean songExistsInEvent(String url, Integer eventId) {
-        List<EventSuggestion> eventSuggestionList = this.findAll();
+        List<EventSuggestion> eventSuggestionList = eventSuggestionRepository.findAllByEventId(eventId);
         for (EventSuggestion eventSuggestion : eventSuggestionList) {
             if (eventSuggestion.getUrl().equals(url) && eventSuggestion.getEvent().getId().equals(eventId)) {
                 return true;
@@ -68,5 +70,21 @@ public class EventSuggestionServiceImpl implements EventSuggestionService {
         }
         return false;
     }
+
+    //TODO: method to set order on change
+//    private void changeEventSuggestionOrderInEvent(EventSuggestion eventSuggestion, Integer newPosition) {
+//        List<EventSuggestion> eventSuggestionList = eventSuggestionRepository.findAllByEventId(eventSuggestion.getEvent().getId());
+//        eventSuggestionList.remove(eventSuggestion);
+//        List<EventSuggestion> eventSuggestionListBeforePosition = new ArrayList<>();
+//        List<EventSuggestion> eventSuggestionListAfterPosition = new ArrayList<>();
+//        int oldPosition = eventSuggestion.getPosition();
+//        eventSuggestionList.forEach(eventSuggestion1 -> {
+//            if (eventSuggestion1.getPosition() < oldPosition) {
+//                eventSuggestionListBeforePosition.add(eventSuggestion1);
+//            } else {
+//                eventSuggestionListAfterPosition.add(eventSuggestion1);
+//            }
+//        });
+//    }
 
 }
